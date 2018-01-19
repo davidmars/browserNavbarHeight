5aaa6109f1881cab2543755a0e22b92291d6dae5
david <d.marsalone@gmail.com>
2018-01-19 19:46:48 +0100

    no more es6, basic test, package json for npm

--- browserNavbarHeight.js
+++ browserNavbarHeight.js
@@ -1,19 +1,21 @@
-/**
- * Calculates the browser navbar height.
- * On desktop it will be zero, on touch devices depends :)
- * @returns {number|*}
- */
-export default function browserNavbarHeight(){
-    let d,r;
-    d=document.createElement("div");
-    d.style.height="100vh";
-    document.body.appendChild(d);
-    r=d.offsetHeight-window.innerHeight;
-    d.remove();
-    return r;
-}
+
+
 
 (function() {
+    /**
+     * Calculates the browser navbar height.
+     * On desktop it will be zero, on touch devices depends :)
+     * @returns {number|*}
+     */
+    function browserNavbarHeight(){
+        var d,r;
+        d=document.createElement("div");
+        d.style.height="100vh";
+        document.body.appendChild(d);
+        r=d.offsetHeight-window.innerHeight;
+        d.remove();
+        return r;
+    }
 
     /**
      * Calculates the browser navbar height. and put this value on...
@@ -25,5 +27,4 @@ export default function browserNavbarHeight(){
     //define css variable
     var html=document.getElementsByTagName("html");
     html[0].style.setProperty("--browser-navbar-height", String(h)+"px");
-    window.scroll(0,h);
 })();
